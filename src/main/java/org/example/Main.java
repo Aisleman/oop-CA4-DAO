b0e3770507bd522cf0fe64513102105bce0c5245
Aisleman <davidailemen7@gmail.com>
2025-02-14 14:59:04 +0000

    Objective 1 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,7 +20,33 @@ public class Main {
 
             Statement statement = conn.createStatement();
 
+            //Query to print out table
+            String sqlQuery = "SELECT * FROM expenses";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
 
+            while (resultSet.next())
+            {
+
+                int expenseID = resultSet.getInt("EXPENSE_ID");
+                String title = resultSet.getString("TITLE");
+                String category = resultSet.getString("CATEGORY");
+                double amount = resultSet.getDouble("AMOUNT");
+                Date date = resultSet.getDate("EXPENSE_DATE");
+
+                System.out.print("ID: " + expenseID);
+                System.out.print("Title: " + title);
+                System.out.print("Category: " + category);
+                System.out.print("Amount: " + amount);
+                System.out.println("Date: " + date);
+            }
+            String sqlQuery2 = "SELECT SUM(amount) FROM expenses";
+
+            ResultSet sumResult = statement.executeQuery(sqlQuery2);
+            while (sumResult.next())
+            {
+                double totalAmount = sumResult.getDouble(1);
+                System.out.println("Total Amount: " + totalAmount);
+            }
             System.out.println("\nFinished - Disconnected from database");
         } catch (SQLException ex) {
             System.out.println("SQL Failed - check MySQL Server is running and that you are using the correct database details");
